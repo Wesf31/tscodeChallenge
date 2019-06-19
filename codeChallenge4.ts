41e9a024aeef2efe7e01a60b8df0cf3fba19f4f3
Wesley Fukui <wes.fukui@gmail.com>
2019-06-18 22:24:49 -0600

    commenting out

--- codeChallenge4.ts
+++ codeChallenge4.ts
@@ -1,5 +1,5 @@
 // code challenge 4
-To do
-1. look up simple way to test methods 
-2. I'm in the red phase still so I need to act like a client that will use my code and write a test
-3. 
+// To do
+// 1. look up simple way to test methods 
+// 2. I'm in the red phase still so I need to act like a client that will use my code and write a test
+// 3. 
